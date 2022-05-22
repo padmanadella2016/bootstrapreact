7110a2bf35dd56b962cffa565545a37f548a8245
pnadella <padmanadella.2016@gmail.com>
2022-05-22 18:22:12 +1000

    test

--- src/About.js
+++ src/About.js
@@ -3,7 +3,7 @@ import React from "react"
 export const About= () =>(
     <div>
         <h2>About Us</h2>
-        <p>loremloremlorem t will create a directory called my-app inside the current folder.
+        <p>loremloremlorem t will create a directory called my-app inside the current folder test.
            Inside that directory, it will generate the initial project structure and install the transitive dependencies:
         </p>
         <p> loremloremlorem t will create a directory called my-app inside the current folder.
